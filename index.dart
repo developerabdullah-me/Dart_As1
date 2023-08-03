a649c6b289f03cae9439e5d45bf4daa7ad064e90
MD ABDULLAH TALHA <abdullahtalha9999@gmail.com>
2023-08-03 20:23:26 +0600

    mmm

---
+++ index.dart
@@ -0,0 +1,19 @@
+void main(){
+var a=70;
+var b=6;
+// Addition
+var addition= a + b;
+print(addition);
+// subtraction
+var subtraction= a - b;
+print(subtraction);
+// multiplication
+var multiplication= a * b;
+print(multiplication);
+// Division
+var division= a / b;
+print(division);
+// Modulus
+var modulus= a % b;
+print(modulus);
+}
